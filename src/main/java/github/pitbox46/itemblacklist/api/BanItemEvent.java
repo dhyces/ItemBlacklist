4904f89efcd62f9cbb7b6b629d871e7d3e1ea078
dhyces <pokmon987@gmail.com>
2023-06-19 12:04:58 -0400

    Update BanItemEvent.java

--- src/main/java/github/pitbox46/itemblacklist/api/BanItemEvent.java
+++ src/main/java/github/pitbox46/itemblacklist/api/BanItemEvent.java
@@ -12,16 +12,23 @@ import javax.annotation.Nullable;
  * with a phase.
  */
 public interface BanItemEvent {
-    Event<BanItemCallback> EVENT = EventFactory.createWithPhases(BanItemCallback.class, banItemCallbacks -> (player, stack) -> {
-        boolean result = true; // By default, should ban. We see if it's blocked by an event.
+    Event<BanItemCallback> EVENT = EventFactory.createWithPhases(BanItemCallback.class, banItemCallbacks -> (player, stack, original) -> {
+        boolean result = original;
         for (BanItemCallback callback : banItemCallbacks) {
-            result = callback.onBannedItem(player, stack);
+            result = callback.onBannedItem(player, stack, original);
         }
         return result;
     }, Event.DEFAULT_PHASE);
 
     @FunctionalInterface
     interface BanItemCallback {
-        boolean onBannedItem(@Nullable Player player, ItemStack stack);
+        /**
+         *
+         * @param player Player that is being checked for permissions
+         * @param stack ItemStack that is being checked for removal
+         * @param original Original state. If it's true, the item would be deleted, if false, the item would not have been deleted
+         * @return Whether to succeed with deletion of the item
+         */
+        boolean onBannedItem(@Nullable Player player, ItemStack stack, boolean original);
     }
 }
