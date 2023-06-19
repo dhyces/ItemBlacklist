82f7bef8612c426426dc10489efe614801eddcd3
dhyces <pokmon987@gmail.com>
2023-06-19 11:59:18 -0400

    1.20 update

--- src/main/java/github/pitbox46/itemblacklist/mixins/TransientCraftingContainerAccessor.java
+++ src/main/java/github/pitbox46/itemblacklist/mixins/TransientCraftingContainerAccessor.java
@@ -1,12 +1,12 @@
 package github.pitbox46.itemblacklist.mixins;
 
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.inventory.TransientCraftingContainer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(CraftingContainer.class)
-public interface CraftingContainerAccessor {
+@Mixin(TransientCraftingContainer.class)
+public interface TransientCraftingContainerAccessor {
 
     @Accessor
     AbstractContainerMenu getMenu();
